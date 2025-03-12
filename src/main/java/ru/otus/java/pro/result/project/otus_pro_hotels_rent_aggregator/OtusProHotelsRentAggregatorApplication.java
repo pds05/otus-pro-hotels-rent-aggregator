6923652cd598acf5025b839746acdcb20415f8fd
Dmitry Petrov <166365976+pds05@users.noreply.github.com>
2025-03-12 13:33:59 +0400

    Initial commit

---
+++ src/main/java/ru/otus/java/pro/result/project/otus_pro_hotels_rent_aggregator/OtusProHotelsRentAggregatorApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.java.pro.result.project.otus_pro_hotels_rent_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusProHotelsRentAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtusProHotelsRentAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/otus/java/pro/result/project/otus_pro_hotels_rent_aggregator/OtusProHotelsRentAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.java.pro.result.project.otus_pro_hotels_rent_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtusProHotelsRentAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
