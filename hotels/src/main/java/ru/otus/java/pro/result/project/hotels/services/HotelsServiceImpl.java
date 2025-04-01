0d94c2fbfe1ea9795e3ec5a189d92cab4e52cc21
Dmitry Petrov <166365976+pds05@users.noreply.github.com>
2025-04-02 00:52:21 +0400

    post query filtering related entities

--- hotels/src/main/java/ru/otus/java/pro/result/project/hotels/services/HotelsServiceImpl.java
+++ hotels/src/main/java/ru/otus/java/pro/result/project/hotels/services/HotelsServiceImpl.java
@@ -6,11 +6,16 @@ import ru.otus.java.pro.result.project.hotels.daos.HotelDao;
 import ru.otus.java.pro.result.project.hotels.dtos.HotelDtoRq;
 import ru.otus.java.pro.result.project.hotels.entities.Hotel;
 import ru.otus.java.pro.result.project.hotels.entities.HotelRoom;
+import ru.otus.java.pro.result.project.hotels.entities.HotelRoomRate;
 import ru.otus.java.pro.result.project.hotels.exceptions.ResourceNotFoundException;
 import ru.otus.java.pro.result.project.hotels.repositories.HotelRoomsRepository;
 import ru.otus.java.pro.result.project.hotels.repositories.HotelsRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,7 +31,8 @@ public class HotelsServiceImpl implements HotelsService {
 
     @Override
     public List<Hotel> findFilterHotels(HotelDtoRq hotelDtoRq) {
-        return hotelDao.getFilterHotels(hotelDtoRq);
+        List<Hotel> hotels = hotelDao.getFilterHotels(hotelDtoRq);
+        return postQueryFilter(hotels, hotelDtoRq);
     }
 
     @Override
@@ -54,4 +60,52 @@ public class HotelsServiceImpl implements HotelsService {
         return hotelRoomsRepository.findWithUserOrdersByIdAndHotel_Id(hotelRoomId, hotelId).orElseThrow(() -> new ResourceNotFoundException("Room not exist"));
     }
 
+    private List<Hotel> postQueryFilter(List<Hotel> hotels, HotelDtoRq filter) {
+        if (filter.getHotelRoomAmenities() != null) {
+            hotels = hotels.stream().peek(hotel -> {
+                Set<HotelRoom> filtered = hotel.getHotelRooms().stream().filter(room ->
+                        filter.getHotelRoomAmenities().stream().allMatch(filterAmenity -> room.getHotelRoomAmenities().stream().anyMatch(roomAmenity -> roomAmenity.getTitle().equalsIgnoreCase(filterAmenity)))
+                ).collect(Collectors.toSet());
+                hotel.setHotelRooms(filtered);
+            }).filter(hotel -> !hotel.getHotelRooms().isEmpty()).toList();
+        }
+        if (filter.getBeds() != null) {
+            hotels = hotels.stream().peek(hotel -> {
+                Set<HotelRoom> filtered = hotel.getHotelRooms().stream().filter(room ->
+                        filter.getBeds().stream().allMatch(filterBed -> room.getHotelRoomBeds().stream().anyMatch(roomBed -> roomBed.getBedType().getTitle().equalsIgnoreCase(filterBed)))
+                ).collect(Collectors.toSet());
+                hotel.setHotelRooms(filtered);
+            }).filter(hotel -> !hotel.getHotelRooms().isEmpty()).toList();
+        }
+        if (filter.getFoods() != null) {
+            hotels = hotels.stream().peek(hotel -> {
+                        Set<HotelRoom> filteredRoom = hotel.getHotelRooms().stream().peek(
+                                hotelRoom -> {
+                                    Set<HotelRoomRate> filteredRate = hotelRoom.getHotelRoomRates().stream().filter(rate ->
+                                            filter.getFoods().stream().allMatch(filterFood -> rate.getFeedType().getTitle().equalsIgnoreCase(filterFood))
+                                    ).collect(Collectors.toSet());
+                                    hotelRoom.setHotelRoomRates(filteredRate);
+                                }
+                        ).filter(hotelRoom -> !hotelRoom.getHotelRoomRates().isEmpty()).collect(Collectors.toSet());
+                        hotel.setHotelRooms(filteredRoom);
+                    }
+            ).filter(hotel -> !hotel.getHotelRooms().isEmpty()).toList();
+        }
+        if (filter.getPriceFrom() != null || filter.getPriceTo() != null) {
+            hotels = hotels.stream().peek(hotel -> {
+                Set<HotelRoom> filteredRoom = hotel.getHotelRooms().stream().peek(
+                        hotelRoom -> {
+                            Set<HotelRoomRate> filteredRate = hotelRoom.getHotelRoomRates().stream().filter(rate ->
+                                    new BigDecimal(Optional.ofNullable(filter.getPriceFrom()).orElse(0)).compareTo(rate.getPrice()) <= 0 &&
+                                            (filter.getPriceTo() == null || new BigDecimal(filter.getPriceTo()).compareTo(rate.getPrice()) >= 0)
+
+                            ).collect(Collectors.toSet());
+                            hotelRoom.setHotelRoomRates(filteredRate);
+                        }
+                ).filter(hotelRoom -> !hotelRoom.getHotelRoomRates().isEmpty()).collect(Collectors.toSet());
+                hotel.setHotelRooms(filteredRoom);
+            }).filter(hotel -> !hotel.getHotelRooms().isEmpty()).toList();
+        }
+        return hotels;
+    }
 }
